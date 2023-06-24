bfa9cdaa9624df271e13f76ec23acc3df27aeaac
Buletiss <vinicius1997feitosa@gmail.com>
2023-06-24 02:53:58 -0300

    style(components): :lipstick: change placeholder holor

--- src/components/Navbar/components/Search/index.tsx
+++ src/components/Navbar/components/Search/index.tsx
@@ -12,9 +12,11 @@ export function Search({ setNameFilter }: SearchProps) {
 
   return (
     <Input
+      color="white"
       maxW="15rem"
       w="100%"
       placeholder="Filtre pelo nome"
+      _placeholder={{ color: "white" }}
       onChange={handleFilterByName}
     />
   );
