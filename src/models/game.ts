63c0713f80216ebb49ece8032a35a592f68ba355
Buletiss <vinicius1997feitosa@gmail.com>
2023-06-24 01:51:53 -0300

    feat(models): :sparkles: add game interface

---
+++ src/models/game.ts
@@ -0,0 +1,10 @@
+export interface Game {
+  id: number;
+  title: string;
+  thumbnail: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  short_description: string;
+  release_date: string;
+}
